2b8fb28920f907a17718b351b2e3ec98b750e4f7
John E. Herreño <jedihe@gmail.com>
2021-04-06 21:53:01 -0500

    utils.sh: prevent fallback composer update
    An empty require-dev will result in a "composer require" call, which behaves as an update. Prevent that by using --no-update

--- utils.sh
+++ utils.sh
@@ -6,7 +6,7 @@ if [[ "$1" = "--add-require-dev" ]];
 then
   # support the composer package syntax (drupal/the_module:version), as well as just the module name.
   MODULE_NAME=$(echo $2 | cut -d'/' -f 2 | cut -d':' -f 1)
-  lando composer require $(cat web/modules/contrib/$MODULE_NAME/composer.json | jq -r '.["require-dev"] | to_entries | map(.key + ":" + .value) | join(" ")');
+  lando composer require --no-update $(cat web/modules/contrib/$MODULE_NAME/composer.json | jq -r '.["require-dev"] | to_entries | map(.key + ":" + .value) | join(" ")');
   # Only support doing one action at a time
   exit 0
 fi
