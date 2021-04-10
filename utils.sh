fed018e17387b8958666e4bf6b7cdcda22bff809
John E. Herreño <jedihe@gmail.com>
2021-04-09 20:04:47 -0500

    utils.sh, --add-require-dev: fix support for 'the_module' syntax

--- utils.sh
+++ utils.sh
@@ -4,8 +4,9 @@
 
 if [[ "$1" = "--add-require-dev" ]];
 then
-  # support the composer package syntax (drupal/the_module:version), as well as just the module name.
-  MODULE_NAME=$(echo $2 | cut -d'/' -f 2 | cut -d':' -f 1)
+  # Support both drupal/the_module:VERSION syntax, as well as just the module name.
+  # We do it by nesting ${2%:*} (removes ':VERSION' suffix), then doing ${VAR#*/} (removes 'drupal/' prefix).
+  MODULE_NAME=$(echo ${${2%:*}#*/})
   lando composer require --no-update $(cat web/modules/contrib/$MODULE_NAME/composer.json | jq -r '.["require-dev"] | to_entries | map(.key + ":" + .value) | join(" ")');
   # Only support doing one action at a time
   exit 0
